fb8e33f61bbd5f93f608591889c8e725ad8b0867
candolfi <jcandolfi32@gmail.com>
2023-08-14 15:36:13 -0700

    Added scoring feature and ability to loop the game 5 times to determine an overall winner

--- javascript.js
+++ javascript.js
@@ -33,43 +33,72 @@ function reAsk() {
 }
 
 function calculateWinner(computer, player) {
+    let winner = 0;
     if(player === "rock") {
         if(computer === "rock") {
             alert("It is a tie!");
+            return (winner = 0);
         }
         else if(computer === "paper") {
             alert("Computer wins! Paper beats Rock");
+            return (winner = -1);
         }
         else {
             alert("You win! Rock beats Scissors");
+            return (winner = 1);
         }
     }
 
     else if(player === "paper") {
         if(computer === "paper") {
             alert("It is a tie!");
+            return (winner = 0);
         }
         else if(computer === "scissors") {
             alert("Computer wins! Scissors beat Paper");
+            return (winner = -1);
         }
         else {
             alert("You win! Paper beats Rock");
+            return (winner = 1);
         }
     }
 
     else {
         if(computer === "scissors") {
             alert("It is a tie!");
+            return (winner = 0);
         }
         else if(computer === "rock") {
             alert("Computer wins! Rock beats Scissors");
+            return (winner = -1);
         }
         else {
             alert("You win! Scissors beat Paper");
+            return (winner = 1);
         }
     }
 }
 
 function rockPaperScissors(){
-    calculateWinner(getComputerChoice(), getPlayerChoice())
+    return calculateWinner(getComputerChoice(), getPlayerChoice())
+}
+
+function game() {
+    let score = 0;
+
+    for(i = 0; i < 5; i++) {
+        score = score + parseInt(rockPaperScissors());
+        console.log(score);
+    }
+
+    if(score > 0) {
+        alert("You've Won");
+    }
+    else if(score < 0) {
+        alert("The computer wins! Better luck next time");
+    }
+    else {
+        alert("It's a tie!")
+    }
 }
